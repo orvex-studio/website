e6bbfd28fdb9c45c92ff8f1d5a1093bdf980978c
maybeizen <me@maybeizen.space>
2026-02-15 15:33:53 -0500

    fix: project file permissions

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.githubusercontent.com",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "github.com",
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+            },
+            {
+                protocol: "https",
+                hostname: "**.githubusercontent.com",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
